78b40d03c704e7b87ebda95ce976633a0a48c011
joshua <mutukujoshua020@gmail.com>
2023-06-19 17:43:24 -0700

    Software is eating the World

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,6 +3,7 @@
 /**
  *
  * main - Prints all possible combinations of single digit numbers.
+(*
  * Return: 0 on success
  */
 int main(void)
@@ -10,15 +11,3 @@ int main(void)
 	int i = '0';
 
 	while (i <= '9')
-	{       
-                putchar(i);
-                if (i != '9')
-                {       
-                        putchar(',');
-                        putchar(' ');
-                }       
-                ++i;
-        }       
-        putchar('\n');
-        return (0)
-}
