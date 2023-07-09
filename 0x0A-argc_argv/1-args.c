78d5bc7741bb31787a2cbbd87a447cc770a6c007
joshua <mutukujoshua020@gmail.com>
2023-07-08 18:41:26 -0700

    Update

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,7 +3,6 @@
 #include "main.h"
 		
 
-		
 /**
 		
  * main - print the number of arguments passed to the program
@@ -11,8 +10,6 @@
  * @argc: number of arguments
 		
  * @argv: array of arguments
-		
- *
 		
  * Return: Always 0 (Success)
 		
